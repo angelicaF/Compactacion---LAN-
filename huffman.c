51c611acf700d09dc9e5ad7f78533801b208eae4
angelicaF <angelica.fallas@ucr.ac.cr>
2015-11-04 16:07:32 -0600

    huffman alternative

--- huffman.c
+++ huffman.c
@@ -1,221 +1,149 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
-#include <math.h>
-
-#define len(x) ((int)log10(x)+1)
-
-/* Node of the huffman tree */
-struct node{
-    int value;
-    char letter;
-    struct node *left,*right;
-};
-
-typedef struct node Node;
-
-/* 81 = 8.1%, 128 = 12.8% and so on. The 27th frequency is the space. Source is Wikipedia */
-int englishLetterFrequencies [27] = {81, 15, 28, 43, 128, 23, 20, 61, 71, 2, 1, 40, 24, 69, 76, 20, 1, 61, 64, 91, 28, 10, 24, 1, 20, 1, 130};
-
-/*finds and returns the small sub-tree in the forrest*/
-int findSmaller (Node *array[], int differentFrom){
-    int smaller;
-    int i = 0;
-
-    while (array[i]->value==-1)
-        i++;
-    smaller=i;
-    if (i==differentFrom){
-        i++;
-        while (array[i]->value==-1)
-            i++;
-        smaller=i;
-    }
-
-    for (i=1;i<27;i++){
-        if (array[i]->value==-1)
-            continue;
-        if (i==differentFrom)
-            continue;
-        if (array[i]->value<array[smaller]->value)
-            smaller = i;
-    }
-
-    return smaller;
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+ 
+typedef struct node_t {
+	struct node_t *left, *right;
+	int freq;
+	char c;
+} *node;
+ 
+struct node_t pool[256] = {{0}};
+node qqq[255], *q = qqq - 1;
+int n_nodes = 0, qend = 1;
+char *code[128] = {0}, buf[1024];
+ 
+node new_node(int freq, char c, node a, node b)
+{
+	node n = pool + n_nodes++;
+	if (freq) n->c = c, n->freq = freq;
+	else {
+		n->left = a, n->right = b;
+		n->freq = a->freq + b->freq;
+	}
+	return n;
 }
-
-/*builds the huffman tree and returns its address by reference*/
-void buildHuffmanTree(Node **tree){
-    Node *temp;
-    Node *array[27];
-    int i, subTrees = 27;
-    int smallOne,smallTwo;
-
-    for (i=0;i<27;i++){
-        array[i] = malloc(sizeof(Node));
-        array[i]->value = englishLetterFrequencies[i];
-        array[i]->letter = i;
-        array[i]->left = NULL;
-        array[i]->right = NULL;
-    }
-
-    while (subTrees>1){
-        smallOne=findSmaller(array,-1);
-        smallTwo=findSmaller(array,smallOne);
-        temp = array[smallOne];
-        array[smallOne] = malloc(sizeof(Node));
-        array[smallOne]->value=temp->value+array[smallTwo]->value;
-        array[smallOne]->letter=127;
-        array[smallOne]->left=array[smallTwo];
-        array[smallOne]->right=temp;
-        array[smallTwo]->value=-1;
-        subTrees--;
-    }
-
-    *tree = array[smallOne];
-
-return;
+ 
+/* priority queue */
+void qinsert(node n)
+{
+	int j, i = qend++;
+	while ((j = i / 2)) {
+		if (q[j]->freq <= n->freq) break;
+		q[i] = q[j], i = j;
+	}
+	q[i] = n;
 }
-
-/* builds the table with the bits for each letter. 1 stands for binary 0 and 2 for binary 1 (used to facilitate arithmetic)*/
-void fillTable(int codeTable[], Node *tree, int Code){
-    if (tree->letter<27)
-        codeTable[(int)tree->letter] = Code;
-    else{
-        fillTable(codeTable, tree->left, Code*10+1);
-        fillTable(codeTable, tree->right, Code*10+2);
-    }
-
-    return;
+ 
+node qremove()
+{
+	int i, l;
+	node n = q[i = 1];
+ 
+	if (qend < 2) return 0;
+	qend--;
+	while ((l = i * 2) < qend) {
+		if (l + 1 < qend && q[l + 1]->freq < q[l]->freq) l++;
+		q[i] = q[l], i = l;
+	}
+	q[i] = q[qend];
+	return n;
 }
-
-/*function to compress the input*/
-void compressFile(FILE *input, FILE *output, int codeTable[]){
-    char bit, c, x = 0;
-    int n,length,bitsLeft = 8;
-    int originalBits = 0, compressedBits = 0;
-
-    while ((c=fgetc(input))!=10){
-        originalBits++;
-        if (c==32){
-            length = len(codeTable[26]);
-            n = codeTable[26];
-        }
-        else{
-            length=len(codeTable[c-97]);
-            n = codeTable[c-97];
-        }
-
-        while (length>0){
-            compressedBits++;
-            bit = n % 10 - 1;
-            n /= 10;
-            x = x | bit;
-            bitsLeft--;
-            length--;
-            if (bitsLeft==0){
-                fputc(x,output);
-                x = 0;
-                bitsLeft = 8;
-            }
-            x = x << 1;
-        }
-    }
-
-    if (bitsLeft!=8){
-        x = x << (bitsLeft-1);
-        fputc(x,output);
-    }
-
-    /*print details of compression on the screen*/
-    fprintf(stderr,"Original bits = %dn",originalBits*8);
-    fprintf(stderr,"Compressed bits = %dn",compressedBits);
-    fprintf(stderr,"Saved %.2f%% of memoryn",((float)compressedBits/(originalBits*8))*100);
-
-    return;
+ 
+/* walk the tree and put 0s and 1s */
+void build_code(node n, char *s, int len)
+{
+	static char *out = buf;
+	if (n->c) {
+		s[len] = 0;
+		strcpy(out, s);
+		code[n->c] = out;
+		out += len + 1;
+		return;
+	}
+ 
+	s[len] = '0'; build_code(n->left,  s, len + 1);
+	s[len] = '1'; build_code(n->right, s, len + 1);
 }
-
-/*function to decompress the input*/
-void decompressFile (FILE *input, FILE *output, Node *tree){
-    Node *current = tree;
-    char c,bit;
-    char mask = 1 << 7;
-    int i;
-
-    while ((c=fgetc(input))!=EOF){
-
-        for (i=0;i<8;i++){
-            bit = c & mask;
-            c = c << 1;
-            if (bit==0){
-                current = current->left;
-                if (current->letter!=127){
-                    if (current->letter==26)
-                        fputc(32, output);
-                    else
-                        fputc(current->letter+97,output);
-                    current = tree;
-                }
-            }
-
-            else{
-                current = current->right;
-                if (current->letter!=127){
-                    if (current->letter==26)
-                        fputc(32, output);
-                    else
-                        fputc(current->letter+97,output);
-                    current = tree;
-                }
-            }
-        }
-    }
-
-    return;
+ 
+void init(const char *s)
+{
+	int i, freq[128] = {0};
+	char c[16];
+ 
+	while (*s) freq[(int)*s++]++;
+ 
+	for (i = 0; i < 128; i++)
+		if (freq[i]) qinsert(new_node(freq[i], i, 0, 0));
+ 
+	while (qend > 2) 
+		qinsert(new_node(0, 0, qremove(), qremove()));
+ 
+	build_code(q[1], c, 0);
 }
-
-/*invert the codes in codeTable2 so they can be used with mod operator by compressFile function*/
-void invertCodes(int codeTable[],int codeTable2[]){
-    int i, n, copy;
-
-    for (i=0;i<27;i++){
-        n = codeTable[i];
-        copy = 0;
-        while (n>0){
-            copy = copy * 10 + n %10;
-            n /= 10;
-        }
-        codeTable2[i]=copy;
-    }
-
-return;
+ 
+void encode(const char *s, char *out)
+{
+	while (*s) {
+		strcpy(out, code[*s]);
+		out += strlen(code[*s++]);
+	}
 }
+ 
+void decode(const char *s, node t)
+{
+	node n = t;
+	while (*s) {
+		if (*s++ == '0') n = n->left;
+		else n = n->right;
+ 
+		if (n->c) putchar(n->c), n = t;
+	}
+ 
+	putchar('\n');
+	if (t != n) printf("garbage input\n");
+}
+ 
+int main(void)
+{
+	int i;
+	FILE *input, *output;
+	input = fopen("text.txt", "r");
 
-int main(){
-    Node *tree;
-    int codeTable[27], codeTable2[27];
-    int compress;
-    char filename[20];
-    FILE *input, *output;
 
-    buildHuffmanTree(&tree);
+	FILE *fp;
+	char  str1[60];
+	int n = 0;
 
-    fillTable(codeTable, tree, 0);
+	fp = fopen("text.txt","r");
+	if(fp == NULL) 
+	{
+	  perror("Error in opening file");
+	  return(-1);
+	}
 
-    invertCodes(codeTable,codeTable2);
+	if( fgets (str1, 60, fp)!=NULL ) 
+	{
+		printf("%c", str1);
+	}
 
-    /*get input details from user*/
-    printf("Type the name of the file to process:n");
-    scanf("%s",filename);
-    printf("Type 1 to compress and 2 to decompress:n");
-    scanf("%d",&compress);
+	fclose(fp);
 
-    input = fopen(filename, "r");
-    output = fopen("output.txt","w");
-
-    if (compress==1)
-        compressFile(input,output,codeTable2);
-    else
-        decompressFile(input,output, tree);
-
-    return 0;
+	const char *str = str1, buf[1024];
+ 
+	init(str);
+	for (i = 0; i < 128; i++)
+		if (code[i]) printf("'%c': %s\n", i, code[i]);
+ 
+	encode(str, buf);
+	printf("encoded: %s\n", buf);
+ 
+	printf("decoded: ");
+	decode(buf, q[1]);
+ 
+	return 0;
 }
